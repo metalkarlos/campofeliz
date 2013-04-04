11436ae47ba0e58c05b0de305bc216ae209d5928
metalkarlos <Metalkarlos0923>
2013-04-04 01:08:52 -0500

    Se crea pdf en carpeta temp de windows. Se agrega <mime-type>application/pdf</mime-type> en web.xml

--- campofeliz/src/com/web/util/Utilities.java
+++ campofeliz/src/com/web/util/Utilities.java
@@ -19,18 +19,19 @@ public class Utilities {
 	public void imprimirJasperPdf(String nombreReporte, Map<String, Object> parametros) throws Exception {
 		
 		try{
-			String rutaReporte = Parametro.RUTA_REPORTES+nombreReporte+".jasper";
+			String rutaReporteOrigen = Parametro.RUTA_REPORTES+nombreReporte+".jasper";
 	        Session session = HibernateUtil.getSessionFactory().openSession();
 	        Connection connection = session.connection();
 	        
-			JasperPrint jasperPrint = JasperFillManager.fillReport(rutaReporte, parametros, connection);//new JREmptyDataSource());
+			JasperPrint jasperPrint = JasperFillManager.fillReport(rutaReporteOrigen, parametros, connection);//new JREmptyDataSource());
 			
 			//crea en disco
-			JasperExportManager.exportReportToPdfFile(jasperPrint, Parametro.RUTA_REPORTES+nombreReporte+".pdf");
+			String rutaReporteDestino = System.getProperty("java.io.tmpdir");
+			JasperExportManager.exportReportToPdfFile(jasperPrint, rutaReporteDestino+nombreReporte+".pdf");
 			
 			//muestra en browser
 			FacesContext.getCurrentInstance().getExternalContext().responseReset(); 
-			FacesContext.getCurrentInstance().getExternalContext().setResponseContentType(FacesContext.getCurrentInstance().getExternalContext().getMimeType(rutaReporte)); 
+			FacesContext.getCurrentInstance().getExternalContext().setResponseContentType(FacesContext.getCurrentInstance().getExternalContext().getMimeType(nombreReporte+".pdf")); 
 			//FacesContext.getCurrentInstance().getExternalContext().setResponseContentLength(contentLength); 
 			FacesContext.getCurrentInstance().getExternalContext().setResponseHeader("Content-disposition", "attachment; filename=\""+nombreReporte+".pdf\"");//inline 
 	        OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
