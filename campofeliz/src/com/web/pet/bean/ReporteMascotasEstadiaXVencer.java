ffc989d5519f3045e500c5580ed97863e7bad5a5
Usuario <Usuario@PERSONAL>
2013-05-31 21:50:52 -0500

    macordova 31/05/2013

---
+++ campofeliz/src/com/web/pet/bean/ReporteMascotasEstadiaXVencer.java
@@ -0,0 +1,74 @@
+package com.web.pet.bean;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+import com.web.pet.global.Parametro;
+import com.web.util.MessageUtil;
+import com.web.util.Utilities;
+
+@ManagedBean
+@Named
+@ViewScoped
+public class ReporteMascotasEstadiaXVencer implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8606123902697883482L;
+	private int anio;
+	
+
+	
+	
+	public int getAnio() {
+		return anio;
+	}
+
+
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+
+	public  ReporteMascotasEstadiaXVencer(){
+		anio =0;
+		
+	}
+	private boolean validarCampos(){
+		boolean ok = true;
+		if(anio ==0){
+			new MessageUtil().showFatalMessage("Datos incompletos!", "Debe ingresar los años a vencer!");
+			ok = false;
+		}else if(anio < 0){
+			new MessageUtil().showFatalMessage("Datos incompletos!", "Los años a ingresar deben ser mayor a cero!");
+			ok = false;
+		}
+		return ok;
+	}	
+	
+	public void imprimir(){
+		try {
+			if (validarCampos()) {
+				String nombreReporte = new String("");
+				String rutaLogo = Parametro.RUTA_IMAGENES_MISCELLANEOUS+"logo_empresa.jpg";
+				Map<String, Object> parametros = new HashMap<String, Object>();
+				parametros.put("P_LOGO", rutaLogo);
+				
+				if(anio >0){
+				  nombreReporte = "MascotasEstadiaxVencer";
+				  parametros.put("P_ANIOS", anio);
+				}
+				new Utilities().imprimirJasperPdf(nombreReporte, parametros);
+			}
+		}catch (Exception e) {
+			new MessageUtil().showFatalMessage("Esto es Vergonzoso!", "Ha ocurrido un error inesperado. Comunicar al Webmaster!");
+		}
+	}
+}
